c56eb3d5c54bf5f4cf34e641a1e823f24874df20
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-27 13:19:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ new_sentence.join(" ")
 end
 
 def reverse_each_word(sentence)
-  sentence.split.collect do |collect_word|
+  sentence.split.collect do |each_word|
     each_word.reverse
   end.join(" ")
 end
