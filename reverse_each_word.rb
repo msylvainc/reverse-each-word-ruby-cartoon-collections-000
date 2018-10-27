809a5dde35d2e2f85c072b34e356e1a400b46493
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-27 12:58:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,9 @@ var str = "Hello there, and how are you?" ;
 var str = "Hi again, just making sure it's reversed!" ;
 var res = str.split(" ");
 
-return_array = []
-greeting.each do|greeting|
-    return_array << greeting.reverse
+return_reverse_each_word = []
+greeting.each do|reverse_each_word|
+    return_reverse_each_word << reverse_each_word.reverse
 
 reverse_each_word("Hello there, and how are you?")
 reverse_each_word("Hi again, just making sure it's reversed!")
