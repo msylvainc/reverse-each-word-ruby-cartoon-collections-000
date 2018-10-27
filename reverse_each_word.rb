a1826e60cb0d8ebcd7c249df2068e071526fee8d
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-27 12:57:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ var res = str.split(" ");
 return_array = []
 greeting.each do|greeting|
     return_array << greeting.reverse
-  end
 
 reverse_each_word("Hello there, and how are you?")
 reverse_each_word("Hi again, just making sure it's reversed!")
+end
