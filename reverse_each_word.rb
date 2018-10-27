5578d4ab8e62cbc805b6c1374d6de3156190304a
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-27 13:12:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,19 @@
-def reverse_each_word
-sentence1 = "Hello there, and how are you?" ;
-sentence2 = "Hi again, just making sure it's reversed!" ;
-var res = sentence.split(" ");
+require 'pry'
 
-return_reverse_each_word = []
-reverse_each_word.each do|reverse_each_word|
-    return_reverse_each_word << reverse_each_word.reverse
+def reverse_each_word(sentence)
+  sentence.split.collect do |each_word|
+    each_word.reverse
+  end.join(" ")
+end
 
-reverse_each_word("Hello there, and how are you?")
-reverse_each_word("Hi again, just making sure it's reversed!")
+def reverse_each_word(sentence)
+   sentence.split.collect {|each_word|each_word.reverse}.join(" ")
+end
+
+def reverse_each_word(sentence)
+new_sentence = []
+sentence.split.each do |each_word|
+new_sentence << each_word.reverse
+end
+new_sentence.join(" ")
 end
