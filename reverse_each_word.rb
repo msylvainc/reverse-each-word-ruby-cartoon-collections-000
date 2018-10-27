3558b0d24926ca9dd4a6cc4f2e20237221a67850
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-27 12:49:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,11 @@ return_array = []
 greeting.each do|greeting|
     return_array << greeting.reverse
   end
-
+  
+  def reverse_each_word(words)
+    words.collect do |x| 
+    end
+    
 def reverse_each_word
 reverse_each_word("Hello there, and how are you?")
 reverse_each_word("Hi again, just making sure it's reversed!")
