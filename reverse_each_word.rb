721387c14ed82e7441f16c7e96480451c1119309
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-27 13:01:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word
 sentence1 = "Hello there, and how are you?" ;
 sentence2 = "Hi again, just making sure it's reversed!" ;
-var res = str.split(" ");
+var res = sentence.split(" ");
 
 return_reverse_each_word = []
 reverse_each_word.each do|reverse_each_word|
